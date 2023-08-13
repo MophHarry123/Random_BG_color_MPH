f780624989d74c192230282bb84796ac0171ff89
Moph Harry <100580089+MophHarry123@users.noreply.github.com>
2023-08-13 20:40:50 +0630

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const changeColorBtn = document.getElementById("changeColorBtn");
 const bgColor = document.getElementById("bgColor");
 
 changeColorBtn.addEventListener("click", () => {
-  const r = Math.floor(Math.random() * 266);
-  const g = Math.floor(Math.random() * 276);
-  const b = Math.floor(Math.random() * 296);
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
 
   const newColor = `rgb(${r}, ${g}, ${b})`;
   bgColor.style.backgroundColor = newColor;
